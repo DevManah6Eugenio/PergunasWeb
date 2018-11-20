62e04ef9b246b45ec60dfc100ef9f71998dd6f69
Manasses-Java <manasses.java@gmail.com>
2018-11-20 18:51:24 -0300

    Criação da entidade Pergunta.java

---
+++ src/main/java/br/com/perguntasweb/entidades/Pergunta.java
@@ -0,0 +1,34 @@
+package br.com.perguntasweb.entidades;
+
+import java.util.Date;
+
+public class Pergunta {
+    
+    String titulo;
+    String descricao;
+    Date criacao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getCriacao() {
+        return criacao;
+    }
+
+    public void setCriacao(Date criacao) {
+        this.criacao = criacao;
+    }
+}
