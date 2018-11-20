bde23595ff8ddaf5c7499a21ca9d29ce076e75b0
Manasses-Java <manasses.java@gmail.com>
2018-11-20 19:13:00 -0300

    Criação da Entidade Perfil.java

---
+++ src/main/java/br/com/perguntasweb/entidades/Perfil.java
@@ -0,0 +1,31 @@
+package br.com.perguntasweb.entidades;
+
+public class Perfil {
+    int codigo;
+    String nome;
+    String descricao;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
